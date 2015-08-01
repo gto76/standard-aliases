3826db3fa238d530914fa398583d42743a434d95
Jure Sorn <sornjure@gmail.com>
2015-08-02 01:51:40 +0200

    Updated parse_rc script that generates the shortcut functions

--- parse_rc.py
+++ parse_rc.py
@@ -5,6 +5,8 @@ import re
 RC_FILENAME = "/home/minerva/.standardrc"
 STANDARD_ALIASES_FILENAME = "/home/minerva/.standard_aliases"
 
+firstLetterToUppercase = lambda s: s[:1].upper() + s[1:] if s else ''
+firstLetterToLowercase = lambda s: s[:1].lower() + s[1:] if s else ''
 
 def getCompletions():
     completions = []
@@ -16,9 +18,15 @@ def getCompletions():
             completions.append(line)
     return completions
 
-
 def glueCommand(command):
-    return re.sub(' ', '', command)
+    words = []
+    for word in command.split():
+        words.append(firstLetterToUppercase(word))
+    words[0] = firstLetterToLowercase(words[0])
+    out = "".join(words)
+    out = re.sub(' ', '', out)
+    out = re.sub('\.', '', out)
+    return out
 
 def main():
     completions = getCompletions()
@@ -26,18 +34,25 @@ def main():
     with open(RC_FILENAME) as f:
         content = f.readlines()
     for line in content:
+        if len(line.strip()) == 0:
+            continue
+        if line.strip().startswith('#'):
+            continue
         tokens = line.strip().split(':')
         if len(tokens) != 2:
             continue 
-        shortcut = tokens[0]
-        command = "__"+glueCommand(tokens[1])
-        print(shortcut+"() {")
-        print("    "+command+" \"$@\"")
-        print("}")
-        completion = [i for i in completions if i.endswith(command)][0]
-        if len(completion) != 0:
-            print(re.sub(command, shortcut, completion))
-        print
+        shortcuts = tokens[0]
+        shortcutTokens = shortcuts.split(',')
+        for shortcut in shortcutTokens:
+            shortcut = shortcut.strip()
+            command = "__"+glueCommand(tokens[1])
+            print(shortcut+"() {")
+            print("    "+command+" \"$@\"")
+            print("}")
+            completion = [i for i in completions if i.endswith(command)]
+            if len(completion) != 0:
+                print(re.sub(command, shortcut, completion[0]))
+            print
 
 if __name__ == '__main__':
     main()
