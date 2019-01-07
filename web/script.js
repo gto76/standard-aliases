4c2f8f07d618802860b504517dcfa557f91f76d5
Jure Šorn <sornjure@gmail.com>
2019-01-07 18:58:53 +0100

    Added webpage

---
+++ web/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  parseMd()
+});
+
+function parseMd() {
+  var GITHUB = 'https://raw.githubusercontent.com/gto76/standard-aliases/master/README.md'
+  jQuery.get(GITHUB, function(text) {
+    var converter = new showdown.Converter()
+    html = converter.makeHtml(text)
+    aDiv = $('#main_container')
+    nodes = $.parseHTML(html)
+    aDiv.after(nodes);
+    insertLinks()
+    d3.selectAll("code").each(function() { hljs.highlightBlock(this); });
+  });
+}
+
+function insertLinks() {
+  $('h2').each(function() {
+    aId = $(this).attr('id')
+    $(this).append('<a href="#'+aId+'" name="'+aId+'">#</a>')
+  })
+}
